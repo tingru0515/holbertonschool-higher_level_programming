01037856ac5a81d108029647c75d89dfbffb16b6
tingru0515 <tingru0515@gmail.com>
2024-01-24 16:53:36 +1100

    two print limit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)), end="")
+            result += chr(ord(char) - 32)
         else:
-            print("{}".format(char), end="")
-    print()
+            result += char
+    print(result)
