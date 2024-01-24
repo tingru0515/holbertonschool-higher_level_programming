057be51cd26cc405821aff7d10d74b6b9be0ee7c
tingru0515 <tingru0515@gmail.com>
2024-01-24 16:57:22 +1100

    format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            result += chr(ord(char) - 32)
+            result += "{}".format(chr(ord(char) - 32))
         else:
-            result += char
+            result += "{}".format(char)
     print(result)
