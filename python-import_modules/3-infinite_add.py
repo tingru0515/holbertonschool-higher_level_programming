36fd3f4e48f9b90b59209722d05cba55e243c8cf
tingru0515 <tingru0515@gmail.com>
2024-01-25 09:11:53 +1100

    q3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 from sys import argv
 if __name__ == "__main__":
     result = 0
-    for i in range(1, len(argv) + 1):
-        result = result + int(argv[i])
+    for i in range(1, len(argv)):
+        result += int(argv[i])
     print("{}".format(result))
