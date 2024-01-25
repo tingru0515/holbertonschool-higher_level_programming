2e3debca3352d5f0ee8d3b96e919a31a894c9a1a
tingru0515 <tingru0515@gmail.com>
2024-01-25 13:20:49 +1100

    q5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for i in range (0, len(my_string)):
-        if my_string[i] != 'c' or my_string[i] != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             new_string = new_string + my_string[i]
     return new_string
