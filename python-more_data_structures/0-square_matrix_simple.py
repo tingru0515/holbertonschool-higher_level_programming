561441e7d3e8af796ea78d6b92c8149d7d0bb415
tingru0515 <tingru0515@gmail.com>
2024-01-29 19:25:57 +1100

    q0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
+    new_matrix = [row[:] for row in matrix]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] ** 2
