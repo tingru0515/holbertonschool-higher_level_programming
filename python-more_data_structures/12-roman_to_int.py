28162f82dfac27f7467450aed8921b51badd44e2
tingru0515 <tingru0515@gmail.com>
2024-01-29 19:14:15 +1100

    q12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, str) is False:
         return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-             'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+             'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+             'CD': 400, 'CM': 900}
     i = 0
     num = 0
     while i < len(roman_string):
