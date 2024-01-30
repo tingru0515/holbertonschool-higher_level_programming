5b4219d07eeaae08d4e33875ce02d16a0fb1556c
tingru0515 <tingru0515@gmail.com>
2024-01-30 11:30:35 +1100

    q0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
+            counter += 1
         print()
-        return i + 1
+        return counter
     except IndexError:
         print("")
-        return i
+        return counter
