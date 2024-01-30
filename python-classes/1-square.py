9af9a835ab7c4b70bfae8536f5f2755bc09ecad5
tingru0515 <tingru0515@gmail.com>
2024-01-31 09:30:36 +1100

    q1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""Module: 0-square.py
+
+This module provides a simple implementation of a Square class.
+
+Classes:
+    Square: Represents a square shape.
+
+Usage:
+    Square = __import__('0-square').Square
+
+    # Example usage
+    square = Square()
+"""
+
 class Square:
     """Represents a square shape.
 
