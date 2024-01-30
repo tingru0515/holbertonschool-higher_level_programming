11f61d42282549fb62b343f5125a6cf4589330a8
tingru0515 <tingru0515@gmail.com>
2024-01-30 18:42:01 +1100

    q4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    
     for i in range(list_length):
         try:
             element_1 = my_list_1[i]
             element_2 = my_list_2[i]
-            if type(element_1) not in (int, float) or type(element_2) not in (int, float):
+            if type(element_1) not in (int, float) or
+            type(element_2) not in (int, float):
                 print("wrong type")
                 result = 0
             else:
