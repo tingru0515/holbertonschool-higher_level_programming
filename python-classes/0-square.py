2beae7f885a5c3543ada3a09875c0f9756359ce5
tingru0515 <tingru0515@gmail.com>
2024-01-30 19:26:40 +1100

    q6

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class Square:
     """Empty class Square that defines a square."""
-    print(__import__("0-square").MyClass.__doc__)
+    #print(__import__("0-square").MyClass.__doc__)
     pass
