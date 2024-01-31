f43bb3e3d59965943b6b2228f75fb411b9b25ec5
tingru0515 <tingru0515@gmail.com>
2024-02-01 09:38:36 +1100

    q4

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-     """Private instance attribute: size"""
+    """Private instance attribute: size"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
