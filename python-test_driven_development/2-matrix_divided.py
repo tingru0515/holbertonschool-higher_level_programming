765296814ad2cc9abc32b83789825dbde6e16c97
tingru0515 <tingru0515@gmail.com>
2024-02-03 10:41:09 +1100

    line too long

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,11 +11,14 @@ def matrix_divided(matrix, div):
       div (int or float): The number to divide each element of the matrix.
 
     Returns:
-      list of lists: A new matrix where each element is divided by div and rounded to 2 decimal places.
+      list of lists: A new matrix where each element is divided \
+      by div and rounded to 2 decimal places.
 
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) \
+       or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -28,8 +31,10 @@ def matrix_divided(matrix, div):
 
     result_matrix = []
     for row in matrix:
-        if not all(isinstance(val, int) for val in row) or not all(isinstance(val, int) for val in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not all(isinstance(val, int) for val in row) \
+           or not all(isinstance(val, int) for val in row):
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
         new_row = [round(element / div, 2) for element in row]
         result_matrix.append(new_row)
 
