5488d49626b0c04c151cd79ec52f5a363be8513d
tingru0515 <tingru0515@gmail.com>
2024-02-05 21:00:12 +1100

    q7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(Rectangle.print_symbol) * self.__width
+        return '\n'.join([str(self.print_symbol) * self.__width
                          for _ in range(self.__height)])
 
     def __repr__(self):
