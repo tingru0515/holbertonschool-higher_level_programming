df198d642d3a98eb70eeabe8284e4b6d006ff380
tingru0515 <tingru0515@gmail.com>
2024-03-12 16:29:15 +1100

    q14

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
