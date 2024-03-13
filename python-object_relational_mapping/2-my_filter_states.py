5bfc4e2d4ce14c807007285deca400343a37a647
tingru0515 <tingru0515@gmail.com>
2024-03-13 19:59:15 +1100

    q2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the table
+where name matches the argument"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name = {} ORDER BY id ASC".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
