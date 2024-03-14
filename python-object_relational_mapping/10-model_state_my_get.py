d028c8b04ea1b1c8f6548eecd69c4cd8ec65d1a6
tingru0515 <tingru0515@gmail.com>
2024-03-14 13:11:10 +1100

    q6

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter_by(name=sys.argv[4]).first()
-    
+
     if state:
         print(state.id)
     else:
